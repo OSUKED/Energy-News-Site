65aa2993b69b6f67764b0c47b83c3392a3b2dea4
AyrtonB <AyrtonBourn@Outlook.com>
2021-01-03 14:38:58 +0000

    removed the post date

--- components/PostMeta.vue
+++ components/PostMeta.vue
@@ -10,11 +10,6 @@
   <span itemprop="name">{{ source }}</span>
   <span v-if="location" itemprop="address"> &nbsp; in {{ location }}</span>
   </div>
-  <div v-if="date" class="post-meta-date">
-  <time pubdate itemprop="datePublished" :datetime="date">
-  {{ resolvedDate }}
-  </time>
-  </div>
 
   <div v-if="tags" class="card-subheading post-meta-tags" itemprop="keywords">
   <PostTag v-for="tag in resolvedTags" :key="tag" :tag="tag" />
@@ -24,7 +19,6 @@
 </template>
 
 <script>
-import dayjs from 'dayjs'
 import PostTag from './PostTag.vue'
 
 export default {
@@ -37,19 +31,11 @@ export default {
     source: {
       type: String,
     },
-    date: {
-      type: String,
-    },
     location: {
       type: String,
     },
   },
   computed: {
-    resolvedDate() {
-      return dayjs(this.date).format(
-        this.$themeConfig.dateFormat || 'ddd, MMM DD YYYY'
-      )
-    },
     resolvedTags() {
       if (!this.tags || Array.isArray(this.tags)) return this.tags
       return [this.tags]
